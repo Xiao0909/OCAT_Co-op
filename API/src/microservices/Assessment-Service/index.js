ba2cd052f01642a32ee900919ef08a9c560e9f9c
Xiao Xiao <xiaox2@mail.uc.edu>
2021-08-31 15:44:24 -0400

    newTask 1
    have no idea why the task1 cannot run correctly. So i tried again and it should working properly. the code is exactly the same as the task1

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -3,9 +3,13 @@ const { Assessments } = require(`../Database`);
 exports.submit = async (assessment) => {
   // use the bookshelf model Assessments from API/src/microservices/Database to save
   // the assessment data in the PostgreSQL database
+  console.log(assessment);
+  await Assessments.save(assessment);
+
 };
 
 exports.getList = () => {
+
   // use the bookshelf model Assessments from API/src/microservices/Database to fetch
   // the assessment data from the PostgreSQL database
   const assessments = [];
--- API/src/routes/Assessment/index.js
+++ API/src/routes/Assessment/index.js
@@ -14,6 +14,8 @@ module.exports = server => {
         // verify that your data is making it here to the API by using console.log(assessment);
         // call the AssessmentService.submit function from the API/src/microservices/Assessment/ and
         // supply the correct parameters
+        console.log(assessment);
+        await AssessmentService.submit(assessment);
 
         ResponseHandler(
           res,
