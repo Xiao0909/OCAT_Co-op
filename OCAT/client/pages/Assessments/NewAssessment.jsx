91678ffa2159d6d6b26a8d624f9ca8c9896d2e6d
Xiao Xiao <xiaox2@mail.uc.edu>
2021-09-02 11:09:51 -0400

    newAssessment
    update with scores

--- OCAT/client/pages/Assessments/NewAssessment.jsx
+++ OCAT/client/pages/Assessments/NewAssessment.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
+// import { useState } from 'react';
 import { AssessmentService } from '../../services/AssessmentService';
 
 export const NewAssessment = () => {
@@ -9,66 +10,65 @@ export const NewAssessment = () => {
   // then onto the OCAT/server/routes/AssessmentAPI express API
   const onSubmit = async (data) => { await AssessmentService.submit(data); };
   const { handleSubmit, register } = useForm();
-  // const onSubmit = (data) => console.log(data);
-
+  // const onSubmit = (data) => setResult(JSON.stringify(data));
+  // const [ counter, setCounter ] = useState(0);
+  // const score = () => { setCounter(counter + 1); };
   return <Form onSubmit={handleSubmit(onSubmit)}>
     <h1>Cat Behavioral Instrument</h1>
     {/* constant questions */}
     <div>
       <div>
         <p>Cat Name</p>
-        <input {...register(`catName`)} placeholder="Cat Name" />
+        <input {...register(`catName`, { maxLength: 10, required: true })} placeholder="Cat Name" />
       </div>
       <br />
       <div>
         <p>Cat Date of Birth</p>
-        <input {...register(`birth`)} placeholder="00/00/0000" />
+        <input {...register(`birth`, { required: true })} placeholder="00/00/0000" />
       </div>
     </div>
     <hr />
     {/* choice questions */}
     <div>
       <div>
-        <p>1. Previous contact with the Cat Judicial System</p>
-        <select {...register(`question1`)} >
-          <option value="0">No</option>
-          <option value="1">Yes</option>
-        </select>
-      </div>
+        <p>
+          1. Previous contact with the Cat Judicial System
+          <br />
 
-      <div>
-        <p>2. Physical altercations with other cats</p>
-        <select {...register(`question2`)} >
-          <option value="0" >0-3 altercations</option>
-          <option value="1">3+ altercations</option>
-        </select>
-      </div>
+          No (score = 0)<br />
 
-      <div>
-        <p>3. Physical altercations with owner(scratching, biting, etc...</p>
-        <select {...register(`question3`)} >
-          <option value="1">10+ altercations</option>
-          <option value="0">1-10 altercations</option>
-        </select>
-      </div>
+          Yes (score = 1)<br />
 
-      <div>
-        <p>4. Plays well with dogs</p>
-        <select {...register(`question4`)} >
-          <option value="1">No</option>
-          <option value="0">Yes</option>
-        </select>
-      </div>
+          2. Physical altercations with other cats<br />
 
-      <div>
-        <p>5. Hisses at strangers</p>
-        <select {...register(`question5`)} >
-          <option value="1">No</option>
-          <option value="0">Yes</option>
-        </select>
+          0-3 altercations (score = 0)<br />
+
+          3+ altercations (score = 1)<br />
+
+          3. Physical altercations with owner (scratching, biting, etc...)<br />
+
+          10+ altercations (score = 1)<br />
+
+          0-10 altercations (score = 0)<br />
+
+          4.  Plays well with dogs<br />
+
+          No (score = 1)<br />
+
+          Yes (score = 0)<br />
+
+          5.  Hisses at strangers<br />
+
+          Yes (score = 1)<br />
+
+          No (score = 0)
+        </p>
+        <p>Your Score:</p>
+        <input {...register(`score`, { required: true })} placeholder="Put Your Score Here" id="inputValue" />
       </div>
 
     </div>
+
     <br />
     <Button variant="primary" type="submit">Submit</Button>
   </Form>;
